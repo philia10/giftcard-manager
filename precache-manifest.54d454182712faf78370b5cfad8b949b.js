b549a75fd2b04fd18b74d54eb848fd53ac4ebf35
Philia Wang <philia1010@gmail.com>
2019-09-30 00:02:46 +1000

    Updates

---
+++ precache-manifest.54d454182712faf78370b5cfad8b949b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d6baee8a87b875efdbbd3dd4dd1443",
+    "url": "/giftcard-manager/index.html"
+  },
+  {
+    "revision": "1c3464d047b92eb1fbe8",
+    "url": "/giftcard-manager/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "11db071cde1d7872de31",
+    "url": "/giftcard-manager/static/js/2.9b0a676c.chunk.js"
+  },
+  {
+    "revision": "1c3464d047b92eb1fbe8",
+    "url": "/giftcard-manager/static/js/main.1689592b.chunk.js"
+  },
+  {
+    "revision": "26e333be523329ab48c3",
+    "url": "/giftcard-manager/static/js/runtime-main.3cf87c63.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/giftcard-manager/static/media/logo.25bf045c.svg"
+  }
+]);
